0347725623f2a19cb87197eea58705cbbd37661b
Andreas Wassmer <menkalinan56@gmail.com>
2018-08-21 14:14:30 +0200

    First implementation of game logic.

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -1,8 +1,28 @@
 const cards = document.querySelectorAll(".memory-card");
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
-    this.classList.toggle("flip");
-    console.log(this);
+    this.classList.add("flip");
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+    } else {
+        hasFlippedCard = false;
+        secondCard = this;
+
+        if (firstCard.dataset.animal === secondCard.dataset.animal) {
+            // it's a match
+            firstCard.removeEventListener("click", flipCard);
+            secondCard.removeEventListener("click", flipCard);
+        } else {
+            setTimeout(() => {
+                firstCard.classList.remove("flip");
+                secondCard.classList.remove("flip");
+            }, 1500);
+        }
+    }
 }
 
 cards.forEach(card => card.addEventListener("click", flipCard));
